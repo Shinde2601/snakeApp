ba51ba26f9c7879f545e6e4fdd9fd1b34fd78098
Pooja <poojashinde24918@gmail.com>
2026-04-22 19:30:19 +0530

    created handle gesture

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,24 +3,41 @@ import { SafeAreaView, StyleSheet } from 'react-native';
 import { JSX } from 'react/jsx-dev-runtime';
 import { Colors } from '../styles/colors';
 import { PanGestureHandler } from 'react-native-gesture-handler';
+import { Coordinate, Direction, GestureEventType } from '../types/types';
+
+const SNAKE_INITIAL_POSITION = [{x: 5, y: 5}];
+const FOOD_INITIAL_POSITION = {x: 5, y: 20};
+const GAME_BOUNDS = {xMin: 0, xMax: 35, yMin: 0, yMax: 63};
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
 
 export default function Game():JSX.Element {
 
-    const handleGesture = (event: any) => {
+    const [direction, setDirection] = React.useState<Direction>(Direction.Right);
+    const [snake, setSnake] = React.useState<Coordinate[]>(SNAKE_INITIAL_POSITION);
+    const [food, setFood] = React.useState<Coordinate>(FOOD_INITIAL_POSITION);
+    const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
+    const [isPaused, setIsPaused] = React.useState<boolean>(false);
+
+    const handleGesture = (event: GestureEventType) => {
         const { translationX, translationY } = event.nativeEvent;
 
         if (Math.abs(translationX) > Math.abs(translationY)) {
             if (translationX > 0) {
                 // moving right
+                setDirection(Direction.Right);
             }
             else {
                 // moving left
+                setDirection(Direction.Left);
             }
         } else {
             if (translationY > 0) {
                 // down
+                setDirection(Direction.Down);
             } else {
                 // up
+                setDirection(Direction.Up);
             }
         }
     };

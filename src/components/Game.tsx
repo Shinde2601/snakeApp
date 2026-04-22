ef9722e5eccc7ae6d1124af355eece77699aaa19
Pooja <poojashinde24918@gmail.com>
2026-04-22 19:17:28 +0530

    added x and y logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,15 +2,36 @@ import * as React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { JSX } from 'react/jsx-dev-runtime';
 import { Colors } from '../styles/colors';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 export default function Game():JSX.Element {
+
+    const handleGesture = (event: any) => {
+        const { translationX, translationY } = event.nativeEvent;
+
+        if (Math.abs(translationX) > Math.abs(translationY)) {
+            if (translationX > 0) {
+                // moving right
+            }
+            else {
+                // moving left
+            }
+        } else {
+            if (translationY > 0) {
+                // down
+            } else {
+                // up
+            }
+        }
+    };
+
+
     return (
-        <SafeAreaView style={styles.container}>
-
-        </SafeAreaView>
-    )
+        <PanGestureHandler onGestureEvent={handleGesture}>
+        <SafeAreaView style={styles.container}>  </SafeAreaView>
+        </PanGestureHandler>
+    );
 }
-
 
 const styles = StyleSheet.create({
     container: {
